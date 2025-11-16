68db434d2b9e74a75045d708194817d7a42c36bc
Namth <tranhuunam23022000@gmail.com>
2025-11-16 21:30:09 +0700

    chuẩn

--- homecare_fe/src/pages/sono/details/bung/bung.constants.js
+++ homecare_fe/src/pages/sono/details/bung/bung.constants.js
@@ -2,7 +2,7 @@ export const STRUCTURE_OPTIONS = {
   Gan: {
     status: [
       "Không thấy bất thường",
-      "Có thoái hóa mỡ lan tỏa ",
+      "Có thoái hóa mỡ lan tỏa",
       "Có nhu mô thô, không đều",
       "Có u máu",
       "Có nang",
@@ -11,60 +11,42 @@ export const STRUCTURE_OPTIONS = {
       "Có tổn thương",
     ],
     position: ["Gan phải", "Gan trái", "Gan phải và gan trái"],
-    needSize: [
-      "Không thấy bất thường",
-      "Có thoái hóa mỡ lan tỏa ",
-      "Có nhu mô thô, không đều",
-      "Có u máu",
-      "Có nang",
-      "Có khối u",
-      "Có nốt vôi hóa",
-      "Có tổn thương",
-    ],
+    needSize: ["Có u máu", "Có nang", "Có khối u"],
   },
 
-  "Đương mật": {
+  "Đường mật": {
     status: [
       "Không thấy bất thường",
       "Có sỏi đường mật",
       "Có giãn đường mật",
-      "Có giãn đường mật",
+      "Có viêm đường mật",
     ],
     position: ["Gan phải", "Gan trái", "Gan phải và gan trái"],
-    needSize: [
-      "Không thấy bất thường",
-      "Có sỏi đường mật",
-      "Có giãn đường mật",
-      "Có giãn đường mật",
-    ],
+    needSize: ["Có sỏi đường mật", "Có giãn đường mật"],
   },
+
   "Túi mật": {
     status: ["Không thấy bất thường", "Có sỏi", "Có polyp", "Có khối u"],
     position: ["Cổ túi mật", "Thân túi mật", "Đáy túi mật"],
-    needSize: ["Không thấy bất thường", "Có sỏi", "Có polyp", "Có khối u"],
+    needSize: ["Có sỏi", "Có polyp", "Có khối u"],
   },
+
   Tụy: {
     status: [
       "Không thấy bất thường",
       "Giãn ống tụy",
       "Vôi hóa tụy",
-      "Khôi u tụy",
-      "Năng tụy",
+      "Khối u tụy",
+      "Nang tụy",
     ],
     position: ["Đầu tụy", "Thân tụy", "Đuôi tụy"],
-    needSize: [
-      "Không thấy bất thường",
-      "Giãn ống tụy",
-      "Vôi hóa tụy",
-      "Khôi u tụy",
-      "Năng tụy",
-    ],
+    needSize: ["Giãn ống tụy", "Vôi hóa tụy", "Khối u tụy", "Nang tụy"],
   },
 
   Lách: {
     status: ["Không thấy bất thường", "Có nang", "Có vôi hóa", "Có khối u"],
     position: ["1/3 trên lách", "1/3 giữa lách", "1/3 dưới lách"],
-    needSize: ["Không thấy bất thường", "Có nang", "Có vôi hóa", "Có khối u"],
+    needSize: ["Có nang", "Có vôi hóa", "Có khối u"],
   },
 
   "Thận phải": {
@@ -77,9 +59,8 @@ export const STRUCTURE_OPTIONS = {
       "Giãn đài bể thận",
       "Giãn niệu quản",
     ],
-    position: ["1/3 trên lách", "1/3 giữa lách", "1/3 dưới lách"],
+    position: ["1/3 trên thận", "1/3 giữa thận", "1/3 dưới thận"],
     needSize: [
-      "Không thấy bất thường",
       "Có sỏi",
       "Có nang",
       "Có khối u",
@@ -88,6 +69,7 @@ export const STRUCTURE_OPTIONS = {
       "Giãn niệu quản",
     ],
   },
+
   "Thận trái": {
     status: [
       "Không thấy bất thường",
@@ -98,9 +80,8 @@ export const STRUCTURE_OPTIONS = {
       "Giãn đài bể thận",
       "Giãn niệu quản",
     ],
-    position: ["1/3 trên lách", "1/3 giữa lách", "1/3 dưới lách"],
+    position: ["1/3 trên thận", "1/3 giữa thận", "1/3 dưới thận"],
     needSize: [
-      "Không thấy bất thường",
       "Có sỏi",
       "Có nang",
       "Có khối u",
@@ -109,9 +90,10 @@ export const STRUCTURE_OPTIONS = {
       "Giãn niệu quản",
     ],
   },
+
   "Bàng quang": {
     status: ["Không thấy bất thường", "Có sỏi", "Có polyp", "Có khối u"],
-    position: ["bàng quang"],
-    needSize: ["Không thấy bất thường", "Có sỏi", "Có polyp", "Có khối u"],
+    position: ["Bàng quang"],
+    needSize: ["Có sỏi", "Có polyp", "Có khối u"],
   },
 };
